5e21fe7e2acfbb0430d7ede3771b86c66cea7f44
GBmixo <52257096+GBmixo@users.noreply.github.com>
2019-10-04 12:59:48 -0700

    Add files via upload

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,8 @@
 require_relative '../config/environment'
 
-puts "hello world"
+#p1 = Person.create({name: "Derek", health: 6})
+#p2 = Person.new({name: "Derek", health: 6})
+old_logger = ActiveRecord::Base.logger
+ActiveRecord::Base.logger = nil
+#add person
+Input.new
